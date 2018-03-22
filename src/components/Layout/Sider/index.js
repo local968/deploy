ff6a0940f9166f18666d4895990b13b655c5939f
vcing <vcing@vcingdeMacBook-Pro.local>
2018-03-22 15:55:12 +0800

    update Sider buttons

--- src/components/Layout/Sider/index.js
+++ src/components/Layout/Sider/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import styles from './styles.module.css';
 import logo from './mr-one-logo-blue.svg';
+import home from './icon-home.svg';
+import help from './icon-help.svg';
+import switchIcon from './switch.svg';
 
 export default () => (
   <aside className={styles.sider}>
     <div className={styles.logo}>
-      <img src={logo} />
+      <img src={logo} alt="logo" />
       <h2 className={styles.mrone}>Mr.one</h2>
     </div>
     <div className={styles.menus}>
-      <a className={styles.home} />
-      <a className={styles.support} />
+      <a className={styles.home}>
+        <img alt="home" src={home} />
+        <h4 className={styles.nav}>Home</h4>
+      </a>
+      <a className={styles.support}>
+        <img alt="support" src={help} />
+        <h4 className={styles.nav}>support</h4>
+      </a>
     </div>
-    <div className={styles.bottom} />
+    <a className={styles.bottom}>
+      <img alt="switch" src={switchIcon} />
+      <h4 className={styles.nav}>
+        Model<br />Training
+      </h4>
+    </a>
   </aside>
 );
