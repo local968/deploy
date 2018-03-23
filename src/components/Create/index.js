2dd167c5647769e7699407c2fd76a4f61f509922
vcing <vcing@vcingdeMacBook-Pro.local>
2018-03-23 17:40:35 +0800

    update create page background

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -4,7 +4,28 @@ import New from './New';
 import Replace from './Replace';
 
 const CreateBackground = ({ children }) => (
-  <div className={styles.background}>{children}</div>
+  <div className={styles.background}>
+    <div className={styles.columns}>
+      {Array(30)
+        .fill(1)
+        .map((value, index) => {
+          const height = Math.random() * index * 4;
+          return (
+            <div
+              key={height}
+              className={styles.column}
+              style={{ height: height + '%' }}
+            />
+          );
+        })}
+      <div className={styles.circle}>
+        <span className={styles.number}>
+          75<span className={styles.percent}>%</span>
+        </span>
+      </div>
+    </div>
+    <div>{children}</div>
+  </div>
 );
 
 const Create = () => (
