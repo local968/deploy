2348b3e0ea841c42add5c94cd9de636883b88ee0
vcing <vcing@vcingdeMacBook-Pro.local>
2018-04-19 14:46:36 +0800

    add config sample file

---
+++ config-sample.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    name: 'newa',
+    host: '127.0.0.1',
+    port: 28015
+  },
+  token_secret: 'NewATechDeMO',
+  port: 3000,
+  queuePeriod: 60,
+  schedulePeriod: 60,
+  maxConcurrencySchedule: 2
+};
