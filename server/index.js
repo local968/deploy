c1ad7bdb045819aede030c54594f31b413bca80c
vcing <2131287@qq.com>
2018-10-17 17:23:58 +0800

    update server port config

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ require('module-alias/register')
 require('events').EventEmitter.prototype._maxListeners = 1000;
 const { server, sessionParser } = require('./http')
 const wssInit = require('./webSocket')
+const config = require('config')
 
 wssInit(server, sessionParser)
 
 // Start the server.
-server.listen(8080, () => console.log('Listening on http://localhost:8080'));
+server.listen(config.port || 8080, () => console.log('Listening on port:' + config.port));
