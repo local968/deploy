4362f464f06033f69e4baf10ea31ce6ae8ec2f4b
yangyang <543984225@qq.com>
2018-10-23 12:23:15 +0800

    添加进入社区按钮

--- src/components/App/Layout.js
+++ src/components/App/Layout.js
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
-import { observer, inject } from "mobx-react";
-import { withRouter } from 'react-router';
-import Route, { LoginRouter } from 'components/App/Route';
+import React, {Component} from "react";
+import { computed } from 'mobx';
+import {observer, inject} from "mobx-react";
+import {withRouter} from 'react-router';
+import Route, {LoginRouter} from 'components/App/Route';
 import styles from './styles.module.css';
-import { Spin } from 'antd';
+import {Spin} from 'antd';
+import Community from 'components/Community';
 
 @withRouter
-@inject('userStore')
+@inject('userStore', 'routing')
 @observer
 export default class Layout extends Component {
+  @computed get community() {
+    return this.props.routing.location.pathname.includes('community')
+  }
+
   render() {
-    const { status } = this.props.userStore;
+    const {routing, userStore} = this.props;
+    const {status} = userStore;
+    // const community = ;
     return <div className={styles.route}>
-      {status === 'init' && <Mask />}
-      {status === 'unlogin' ? <LoginRouter /> : <Route />}
+      {!this.community && status === 'init' && <Mask/>}
+      {!this.community && status === 'unlogin' ? <LoginRouter/> : <Route/>}
+      {this.community && <Community/>}
     </div>
   }
 }
---
+++ src/components/Community/index.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import styles from './styles.module.css';
+import {inject, observer} from 'mobx-react';
+import moment from 'moment';
+import {Bread, Select, Pagination, Switch, Search} from 'components/Common';
+import {Menu, Dropdown, Icon} from 'antd';
+import communityLanguage from './communityLanguage.png'
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer"
+         href="http://localhost:3000?language=zh-CN">中文社区</a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer"
+         href="http://localhost:3000?language=en-US">community for english</a>
+    </Menu.Item>
+  </Menu>
+);
+
+export default class Community extends Component {
+
+  render() {
+    return (
+      <div className={styles.content}>
+        <div className={styles.header}>R2.ai Community - Your Intelligent Partner in AI</div>
+        <div className={styles.label}>Use the same product login ID and password to access our AI community for</div>
+        <div className={styles.funcs}>
+          <div>Rich product resources</div>
+          <div>In-depth case studies</div>
+          <div>Quotes from experts</div>
+          <div>Q&A with data scientists</div>
+          <div>Reviews from users</div>
+          <div>First to try our products</div>
+        </div>
+        <div className={styles.endLabel}>After product account expires, you can still use the account to acess our
+          community to learn more!
+        </div>
+
+        <div className={styles.selectContent}>
+          <img className={styles.imgs} src={communityLanguage} alt="communityImage"/>
+          <Dropdown overlay={menu} placement="bottomCenter">
+            <a className="ant-dropdown-link" href="#" className={styles.selectText}>
+              Enter the community <Icon type="down"/>
+            </a>
+          </Dropdown>
+        </div>
+      </div>
+    )
+  }
+}
