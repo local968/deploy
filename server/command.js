07877a44d1cc7ecb7fd2aa6578d3ef7bc5db1efc
vcing <2131287@qq.com>
2018-11-08 12:15:39 +0800

    add command debug mode

--- server/command.js
+++ server/command.js
@@ -1,13 +1,18 @@
 const { redis, pubsub } = require('redis')
+const moment = require('moment')
 const config = require("../config")
 const FINISH = 1
 const SEND = 2
+
+const DEBUG = process.env.DEPLOY_DEBUG
 
 const command = (command, callback) => {
   const returnPromise = new Promise((resolve, reject) => {
     const requestId = command.requestId
     if (!requestId) return reject(new Error('no request id'))
-    pubsub.lpush(config.requestQueue, JSON.stringify(command))
+    pubsub.lpush(config.requestQueue, JSON.stringify(command)).then(length => {
+      if (DEBUG) console.log(`[command@${moment().format('YYYY-MM-DD HH-mm-ss')}]request(${length}):`, command)
+    })
     if (!callback) return pubsub.once(requestId, resolve)
     const _callback = async (result) => {
       const returnValue = await callback(result)
@@ -28,6 +33,7 @@ const watchQueue = async () => {
     result = await pubsub.rpop(config.resultQueue)
     if (result === null) return
     result = JSON.parse(result)
+    if (DEBUG) console.log(`[command@${moment().format('YYYY-MM-DD HH-mm-ss')}]result:`, result)
     const requestId = result.requestId
     pubsub.emit(requestId, result)
   }
