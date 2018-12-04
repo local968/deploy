e8a526ef0e5e66f7821ebe199e553f4a0e858f2a
vcing <2131287@qq.com>
2018-12-04 11:55:35 +0800

    update ErrorBoundary

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import createBrowserHistory from 'history/createBrowserHistory';
-import {Provider} from 'mobx-react';
-import {RouterStore, syncHistoryWithStore} from 'mobx-react-router';
-import {Router} from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import { Router } from 'react-router-dom';
 import Layout from 'components/App/Layout';
 import Sider from 'components/Layout/Sider';
 import Header from 'components/Layout/Header';
 import Community from 'components/Community';
 import Support from 'components/Support';
+import ErrorBoundary from 'components/Common/ErrorBoundary';
 import Stores from 'stores';
 import styles from './styles.module.css';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const browserHistory = createBrowserHistory();
 const routingStore = new RouterStore();
@@ -21,25 +22,29 @@ const stores = {
 };
 
 const Product = () => <div className={styles.main}>
-  <Header/>
-  <Layout/>
+  <Header />
+  <ErrorBoundary>
+    <Layout />
+  </ErrorBoundary>
 </div>;
 
 class App extends Component {
   render() {
     return (
-      <Provider {...stores}>
-        <Router history={history}>
-          <div className={styles.app}>
-            <Sider/>
-            <Switch>
-              <Route exact path="/community" component={Community}/>
-              <Route exact path="/support" component={Support}/>
-              <Route component={() => <Product/>}/>
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+      <ErrorBoundary>
+        <Provider {...stores}>
+          <Router history={history}>
+            <div className={styles.app}>
+              <Sider />
+              <Switch>
+                <Route exact path="/community" component={Community} />
+                <Route exact path="/support" component={Support} />
+                <Route component={() => <Product />} />
+              </Switch>
+            </div>
+          </Router>
+        </Provider>
+      </ErrorBoundary>
     );
   }
 }
---
+++ src/components/Common/ErrorBoundary/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // You can also log error messages to an error reporting service here
+  }
+
+  refresh = window.location.reload.bind(window.location)
+
+  goback = window.history.back.bind(window.history)
+
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong. Please <a onClick={this.refresh}>refresh</a> or <a onClick={this.goback}>go back</a></h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }
+}
