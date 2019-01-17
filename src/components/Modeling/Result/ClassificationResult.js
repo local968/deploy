d6995fbc827ae151aec58390c1f1c4fd5eca91b9
congcong <congcong.dai@unidt.com>
2019-01-17 16:43:14 +0800

    fix cost based bug

--- src/components/Modeling/Result/ClassificationResult.js
+++ src/components/Modeling/Result/ClassificationResult.js
@@ -20,7 +20,7 @@ export default class ClassificationView extends Component {
   onChange = e => {
     const criteria = e.target.value
     this.showCost = criteria === 'cost'
-    const data = { criteria }
+    const data = { criteria, selectId: '' }
     if (!this.showCost) {
       const { models } = this.props
       this.costOption = { TP: 0, FN: 0, FP: 0, TN: 0 }
