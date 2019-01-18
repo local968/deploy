4a2f6d7a0d68c40271112c084cd39de712e01f27
vcing <2131287@qq.com>
2019-01-18 16:01:44 +0800

    fix 133

--- src/components/User/ForgetPassword/index.js
+++ src/components/User/ForgetPassword/index.js
@@ -8,11 +8,14 @@ import { inject, observer } from 'mobx-react'
 class ForgetPassword extends Component {
 
   @observable isSend = false
+  @observable isResend = false
   @observable email = ''
 
-  send = action(() => {
-    this.isSend = true
+  send = (resend = false) => action(() => {
+    if (this.isSend && this.isResend) return
     this.props.userStore.forgetPassword(this.email)
+    this.isSend = true
+    this.isResend = resend
   })
 
   onChange = action(event => {
@@ -22,14 +25,14 @@ class ForgetPassword extends Component {
   render() {
     const resultTab = <div className={styles.block}>
       <h3 className={styles.title}>Reset Your Password</h3>
-      <p className={styles.description}>We have sent a reset password email to {this.email}. Please click the resepassword link to set your new password. <br /><br /><br />Didn’t receive the email yet? Please check your spam folder, or <a onClick={this.send}>resend</a> the email.</p>
+      <p className={styles.description}>We have sent a reset password email to {this.email}. Please click the reset password link to set your new password. <br /><br /><br />Didn't receive the email yet? Please check your spam folder, or <a onClick={this.send(true)}>resend</a> the email.</p>
     </div>
 
     const inputTab = <div className={styles.block}>
       <h3 className={styles.title}>Forget Password</h3>
-      <p className={styles.description}>Please enter your email address you registered with us, and we’ll send you instruction on how to reset your password</p>
+      <p className={styles.description}>Please enter your email address you registered with us, and we'll send you instruction on how to reset your password</p>
       <input className={styles.input} type='text' value={this.email} onChange={this.onChange} placeholder='Email Address' />
-      <a className={styles.submit} onClick={this.send}>Send</a>
+      <a className={styles.submit} onClick={this.send()}>Send</a>
     </div>
     return this.isSend ? resultTab : inputTab
   }
