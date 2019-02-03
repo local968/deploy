cd5e88ae9a0b11384eaef7b210bf1a94cf677af5
vcing <2131287@qq.com>
2019-02-03 10:56:22 +0800

    update log

--- server/http.js
+++ server/http.js
@@ -14,6 +14,9 @@ const fs = require('fs')
 
 const app = express();
 
+// fetch ip
+app.set('trust proxy', true);
+
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
