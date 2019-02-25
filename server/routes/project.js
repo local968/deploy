1f6a5881eddf8175afc4cb992fdc6840d693ea2e
congcong <congcong.dai@unidt.com>
2019-02-25 15:14:27 +0800

    fix time

--- server/routes/project.js
+++ server/routes/project.js
@@ -158,12 +158,13 @@ function addSettingModel(userId, projectId) {
 function createModel(userId, id, modelId, params) {
   const mid = uuid.v4()
   const pipeline = redis.pipeline();
-  pipeline.hmset(`project:${id}:model:${modelId}`, mapObjectToArray({ ...params, id: modelId, mid, createTime: moment().unix() }))
+  const saveData = { ...params, id: modelId, mid, createTime: moment().unix() }
+  pipeline.hmset(`project:${id}:model:${modelId}`, mapObjectToArray(saveData))
   pipeline.sadd(`project:${id}:models`, modelId)
   return pipeline.exec().then(list => {
     const err = list.find(([error]) => !!error);
     const data = err ? { status: 412, message: "create model error" } : { status: 200, message: "ok" }
-    const result = { ...data, model: { ...params, id: modelId, mid }, id }
+    const result = { ...data, model: saveData, id }
     wss.publish(`user:${userId}:projects`, result)
     return result
   })
