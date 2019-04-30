55e9957f8f73dcc41ee08a623af762361c26427b
lyz <yangzhe.lu@r2.ai>
2019-04-30 21:21:41 +0800

    update

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import { Bread } from 'components/Common';
-import { Route, Redirect } from 'react-router-dom';
-import { inject, observer } from 'mobx-react';
-import { runInAction } from 'mobx';
-import classnames from 'classnames';
-import deploymentIcon from './icon-tab-deployment.svg';
-import operationIcon from './icon-tab-operation.svg';
-import performanceIcon from './icon-tab-performance.svg';
-import statusIcon from './icon-tab-performance.svg';
-import Deployment from './Deployment';
-import Operation from './Operation';
-import Performance from './Performance';
-import Status from './Status';
-import styles from './styles.module.css';
+import React, { Component } from "react";
+import { Bread } from "components/Common";
+import { Route, Redirect } from "react-router-dom";
+import { inject, observer } from "mobx-react";
+import { runInAction } from "mobx";
+import classnames from "classnames";
+import deploymentIcon from "./icon-tab-deployment.svg";
+import operationIcon from "./icon-tab-operation.svg";
+import performanceIcon from "./icon-tab-performance.svg";
+import statusIcon from "./icon-tab-performance.svg";
+import Deployment from "./Deployment";
+import Operation from "./Operation";
+import Performance from "./Performance";
+import Status from "./Status";
+import styles from "./styles.module.css";
 
-@inject('routing', 'deploymentStore')
+@inject("routing", "deploymentStore")
 @observer
 export default class Detail extends Component {
   render() {
     const { routing, match, location, deploymentStore } = this.props;
     runInAction(() => (deploymentStore.currentId = match.params.id));
-    // const cd = deploymentStore.currentDeployment || {};
+    const cd = deploymentStore.currentDeployment || {};
+    const isUnsupervised = ["Clustering", "Outlier"].includes(cd.modelType);
     return (
       <div className={styles.detail}>
-        <Bread list={['Home']} />
+        <Bread list={["Home"]} />
         <div className={styles.tabs}>
           <div
             className={classnames([styles.tab, styles.deployment], {
-              [styles.active]: location.pathname.indexOf('deployment') >= 0
+              [styles.active]: location.pathname.indexOf("deployment") >= 0
             })}
             onClick={() =>
               routing.push(`/deploy/project/${match.params.id}/deployment`)
@@ -42,7 +43,7 @@ export default class Detail extends Component {
           </div>
           <div
             className={classnames([styles.tab, styles.operation], {
-              [styles.active]: location.pathname.indexOf('operation') >= 0
+              [styles.active]: location.pathname.indexOf("operation") >= 0
             })}
             onClick={() =>
               routing.push(`/deploy/project/${match.params.id}/operation`)
@@ -51,32 +52,37 @@ export default class Detail extends Component {
             <img className={styles.icon} src={operationIcon} alt="operation" />
             <span className={styles.text}>Operation Monitor</span>
           </div>
-          <div
-            className={classnames([styles.tab, styles.performance], {
-              [styles.active]: location.pathname.indexOf('performance') >= 0
-            })}
-            onClick={() =>
-              routing.push(`/deploy/project/${match.params.id}/performance`)
-            }
-          >
-            <img
-              className={styles.icon}
-              src={performanceIcon}
-              alt="performance"
-            />
-            <span className={styles.text}>Performance Monitor</span>
-          </div>
-          <div
-            className={classnames([styles.tab, styles.status], {
-              [styles.active]: location.pathname.indexOf('status') >= 0
-            })}
-            onClick={() =>
-              routing.push(`/deploy/project/${match.params.id}/status`)
-            }
-          >
-            <img className={styles.icon} src={statusIcon} alt="status" />
-            <span className={styles.text}>Performance Status</span>
-          </div>
+          {!isUnsupervised && (
+            <div
+              className={classnames([styles.tab, styles.performance], {
+                [styles.active]: location.pathname.indexOf("performance") >= 0
+              })}
+              onClick={() =>
+                routing.push(`/deploy/project/${match.params.id}/performance`)
+              }
+            >
+              <img
+                className={styles.icon}
+                src={performanceIcon}
+                alt="performance"
+              />
+              <span className={styles.text}>Performance Monitor</span>
+            </div>
+          )}
+
+          {!isUnsupervised && (
+            <div
+              className={classnames([styles.tab, styles.status], {
+                [styles.active]: location.pathname.indexOf("status") >= 0
+              })}
+              onClick={() =>
+                routing.push(`/deploy/project/${match.params.id}/status`)
+              }
+            >
+              <img className={styles.icon} src={statusIcon} alt="status" />
+              <span className={styles.text}>Performance Status</span>
+            </div>
+          )}
         </div>
         <div className={styles.content}>
           <Route
@@ -88,11 +94,16 @@ export default class Detail extends Component {
           />
           <Route path="/deploy/project/:id/deployment" component={Deployment} />
           <Route path="/deploy/project/:id/operation" component={Operation} />
-          <Route
-            path="/deploy/project/:id/performance"
-            component={Performance}
-          />
-          <Route path="/deploy/project/:id/status" component={Status} />
+          {!isUnsupervised && (
+            <Route
+              path="/deploy/project/:id/performance"
+              component={Performance}
+            />
+          )}
+
+          {!isUnsupervised && (
+            <Route path="/deploy/project/:id/status" component={Status} />
+          )}
         </div>
         {/* <div className={styles.enableWrap}>
           <span className={styles.enableText}>Enable</span>
