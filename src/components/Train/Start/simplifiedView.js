6883ff8282cbd902711b87966b8b47c0b6a18f0f
lyz <yangzhe.lu@r2.ai>
2019-05-08 18:35:13 +0800

    update

--- src/components/Train/Start/simplifiedView.js
+++ src/components/Train/Start/simplifiedView.js
@@ -221,7 +221,7 @@ export default class SimplifiedView extends Component {
             <Icon type="loading" />
           </div> :
           <div className={styles.tableBody}>
-            {allVariables.map((h, i) => {
+            {allVariables.filter((h)=>colType[h] !== "Raw").map((h, i) => {
               const data = { ...dataViews, ...newVariableViews }[h]
               const map = targetMap || {};
               return <SimplifiedViewRow key={i} value={h} data={data} map={map} weight={(weights || {})[h]} handleWeight={this.handleWeight(h)} colType={variableType} project={project} isChecked={checkedVariables.includes(h)} handleCheck={this.handleCheck.bind(null, h)} lines={Math.min(Math.floor(totalLines * 0.95), 1000)} id={id} />
