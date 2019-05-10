161bbf0ec3ff106d6c4b06ec2f5fdbeec7a5371d
jindw <dawei.jin@r2.ai>
2019-05-10 14:32:19 +0800

    RL-1897 聚类、异常值检测模型下的模型选择页面，更改模型后，图表的数据未及时更新

--- src/components/Charts/ISO2.jsx
+++ src/components/Charts/ISO2.jsx
@@ -19,10 +19,6 @@ const color = ['#6e698f','#d5d4df','#367de9','#f3ce31'];//背景(开始)/背景(
 export default class Iso extends PureComponent{
     constructor(props){
         super(props);
-        // const {featureImportance} = props.models[0];
-        // const list = Object.entries(featureImportance).sort((b,a)=>a[1]-b[1])
-        // const var1 = list[0][0];
-        // const var2 = list[1][0];
         this.state = {
             ready:false,
             slider_value:0,
@@ -37,7 +33,7 @@ export default class Iso extends PureComponent{
             var2:'',
         };
 
-        this.updatePoint = debounce(this.updatePoint, 1000)
+        this.updatePoint = debounce(this.updatePoint, 280)
     }
     
     componentWillReceiveProps(nextProps) {
@@ -46,8 +42,8 @@ export default class Iso extends PureComponent{
         }
     }
 
-    async componentDidMount() {
-        const { url,default_point:point=0,models} = this.props;
+    async componentDidMount(url=this.props.url) {
+        const { default_point:point=0,models} = this.props;
         const {featureImportance} = models[0];
         const list = Object.entries(featureImportance).sort((b,a)=>a[1]-b[1]);
         const var1 = list[0][0];
@@ -209,9 +205,9 @@ export default class Iso extends PureComponent{
                 // toolbox: {
                 //     feature: {
                 //         dataZoom: {},
-                //         brush: {
-                //             type: ['rect'],
-                //         },
+                //         // brush: {
+                //         //     type: ['rect'],
+                //         // },
                 //     },
                 // },
                 series,
@@ -248,10 +244,10 @@ export default class Iso extends PureComponent{
 
     save(){
         const {show_name} = this.state;
-        const {x_name,y_name} = show_name;
+        const {var1,var2} = show_name;
         this.setState({
-            x_name,
-            y_name,
+            var1,
+            var2,
         })
         //this.props.changeUrl(x_name,y_name)
     }
