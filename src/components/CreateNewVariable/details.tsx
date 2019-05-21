0c81a92deee7041893199927455778b51663b064
yangyang <543984225@qq.com>
2019-05-21 14:30:11 +0800

    description-senior

--- src/components/CreateNewVariable/details.tsx
+++ src/components/CreateNewVariable/details.tsx
@@ -42,25 +42,5 @@ export default [
   {
     "name": "Mean",
     "value": "语法: mean(@var1, @var2, @var3, ...)",
-  },
-  {
-    "name": "Concat",
-    "value": "语法: Concat(@var1, @var2, @var3,...p1, p2...)",
-  },
-  {
-    "name": "Diff",
-    "value": "语法: Diff(@var1, @var2, @var3,...row1,row2,...)",
-  },
-  {
-    "name": "Accumulate",
-    "value": "语法: Accumulate(@var1, @var2, @var3,...)",
-  },
-  {
-    "name": "Quantile_bin",
-    "value": "语法: Quantile_bin(@var1, @var2, @var3,b, type1, type2)",
-  },
-  {
-    "name": "Custom_Quantile_bin",
-    "value": "语法: Custom_Quantile_bin(@var, [range_list1], [range_list2]...)",
   }
 ]
