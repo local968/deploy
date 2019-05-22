99b4069bc37be7a27633bb14ba9990e3af0cc338
zl <zl940722@gmail.com>
2019-05-22 19:51:16 +0800

    update

--- src/components/Report/index.js
+++ src/components/Report/index.js
@@ -102,12 +102,12 @@ class Report extends Component {
 
   render() {
     const { projectStore: { list } } = this.props
-    const { selectModel: model } = list
-    // const { score: { validateScore: vs, holdoutScore: hs }, fitIndex, chartData } = model
-    // const { roc, rocHoldout: roch } = chartData || {}
-    // const { targetArray, targetColMap, renameVariable } = list
-    // const [v0, v1] = !targetArray.length ? Object.keys(targetColMap) : targetArray;
-    // const [no, yes] = [renameVariable[v0] || v0, renameVariable[v1] || v1];
+    const { selectModel: model } = list[0]
+    const { score: { validateScore: vs, holdoutScore: hs }, fitIndex, chartData } = model
+    const { roc, rocHoldout: roch } = chartData || {}
+    const { targetArray, targetColMap, renameVariable } = list[0]
+    const [v0, v1] = !targetArray.length ? Object.keys(targetColMap) : targetArray;
+    const [no, yes] = [renameVariable[v0] || v0, renameVariable[v1] || v1];
 
     console.log(list[0] , 'listlistlistlistlistlist' ,list[0].name)
     return (
@@ -152,117 +152,110 @@ class Report extends Component {
         {/*</div>}*/}
 
 
+        <div className={styles.modelResult}>
+          {this.isShow('modelResult') && <h1 className={styles.title}>{EN.ModelResult}</h1>}
+          {this.checkBox('modelResult')}
+          {this.isShow('modelName') && <div className={classnames(styles.block, styles.marginZero)}>
+            {this.checkBox('modelName')}
+            <h3 className={styles.blockTitle}>{EN.ModelName}: <span className={styles.modelName}>{list[0].selectModel.name}</span></h3>
+            {/* {project.problemType === 'Classification' && <div className={styles.blockRow}><ClassificationPerformance project={project} /></div>} */}
+            {/* {project.problemType === 'Regression' && <div className={classnames(styles.blockRow, styles.performance)}><RegressionPerformance project={project} /></div>} */}
+          </div>}
+          {this.isShow('metrics') && <div className={classnames(styles.block, styles.VariableImpact)}>
+            {this.checkBox('metrics')}
+            <h3 className={styles.blockTitle}>{EN.Metrics}</h3>
+            {/*{project.problemType === 'Regression' && <div className={styles.metrics}>*/}
+            {/*  <div className={classnames(styles.metricsRow, styles.metricsHeader)}>*/}
+            {/*    <span className={styles.metricsCell}></span>*/}
+            {/*    <span className={styles.metricsCell}>R²</span>*/}
+            {/*    <span className={styles.metricsCell}>Adjusted R²</span>*/}
+            {/*    <span className={styles.metricsCell}>MSE</span>*/}
+            {/*    <span className={styles.metricsCell}>RMSE</span>*/}
+            {/*    <span className={styles.metricsCell}>NRMSE</span>*/}
+            {/*    <span className={styles.metricsCell}>MAE</span>*/}
+            {/*  </div>*/}
+            {/*  <div className={styles.metricsRow}>*/}
+            {/*    <span className={styles.metricsCell}>{EN.Validation}</span>*/}
+            {/*    <span className={styles.metricsCell} title={vs.r2}>{formatNumber(vs.r2)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={vs.adjustR2}>{formatNumber(vs.adjustR2)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={vs.mse}>{formatNumber(vs.mse)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={vs.rmse}>{formatNumber(vs.rmse)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={vs.nrmse}>{formatNumber(vs.nrmse)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={vs.mae}>{formatNumber(vs.mae)}</span>*/}
+            {/*  </div>*/}
+            {/*  <div className={styles.metricsRow}>*/}
+            {/*    <span className={styles.metricsCell}>{EN.Holdout}</span>*/}
+            {/*    <span className={styles.metricsCell} title={hs.r2}>{formatNumber(hs.r2)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={hs.adjustR2}>{formatNumber(hs.adjustR2)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={hs.mse}>{formatNumber(hs.mse)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={hs.rmse}>{formatNumber(hs.rmse)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={hs.nrmse}>{formatNumber(hs.nrmse)}</span>*/}
+            {/*    <span className={styles.metricsCell} title={hs.mae}>{formatNumber(hs.mae)}</span>*/}
+            {/*  </div>*/}
+            {/*</div>}*/}
+            {list[0].problemType === 'Classification' && <div className={styles.metrics}>
+              <div className={classnames(styles.metricsRow, styles.metricsHeader)}>
+                <span className={styles.metricsCell}></span>
+                <span className={styles.metricsCell}>AUC</span>
+                <span className={styles.metricsCell}>Cutoff</span>
+                <span className={styles.metricsCell}>{EN.Accuracy}</span>
+                <span className={styles.metricsCell}>{EN.Precision}</span>
+                <span className={styles.metricsCell}>{EN.Recall}</span>
+                <span className={styles.metricsCell}>F1 Score</span>
+                <span className={styles.metricsCell}>KS</span>
+                <span className={styles.metricsCell}>{EN.LogLoss}</span>
+              </div>
+              <div className={styles.metricsRow}>
+                <span className={styles.metricsCell}>{EN.Validation}</span>
+                <span className={styles.metricsCell} title={vs.auc}>{formatNumber(vs.auc)}</span>
+                <span className={styles.metricsCell} title={roc.Threshold[fitIndex]}>{formatNumber(roc.Threshold[fitIndex])}</span>
+                <span className={styles.metricsCell} title={model.accValidation}>{formatNumber(model.accValidation)}</span>
+                <span className={styles.metricsCell} title={roc.Precision[fitIndex]}>{formatNumber(roc.Precision[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roc.Recall[fitIndex]}>{formatNumber(roc.Recall[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roc.F1[fitIndex]}>{formatNumber(roc.F1[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roc.KS[fitIndex]}>{formatNumber(roc.KS[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roc.LOGLOSS[fitIndex]}>{formatNumber(roc.LOGLOSS[fitIndex])}</span>
+              </div>
+              <div className={styles.metricsRow}>
+                <span className={styles.metricsCell}>{EN.Holdout}</span>
+                <span className={styles.metricsCell} title={hs.auc}>{formatNumber(hs.auc)}</span>
+                <span className={styles.metricsCell} title={roch.Threshold[fitIndex]}>{formatNumber(roch.Threshold[fitIndex])}</span>
+                <span className={styles.metricsCell} title={model.accHoldout}>{formatNumber(model.accHoldout)}</span>
+                <span className={styles.metricsCell} title={roch.Precision[fitIndex]}>{formatNumber(roch.Precision[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roch.Recall[fitIndex]}>{formatNumber(roch.Recall[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roch.F1[fitIndex]}>{formatNumber(roch.F1[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roch.KS[fitIndex]}>{formatNumber(roch.KS[fitIndex])}</span>
+                <span className={styles.metricsCell} title={roch.LOGLOSS[fitIndex]}>{formatNumber(roch.LOGLOSS[fitIndex])}</span>
+              </div>
+              <div className={styles.titles}>
+                <div className={styles.metricsTitle}>{EN.ConfusionMatrix}</div>
+                <div className={styles.metricsTitle}>{EN.CostBased}</div>
+              </div>
+              <PredictTable model={model} yes={yes} no={no} project={list[0]} />
+            </div>}
+          </div>}
 
-        {/* {this.isShow('correlationMatrix') && <div className={styles.block}>
-        {this.checkBox('correlationMatrix')}
-        <h3 className={styles.blockTitle}>Correlation Matrix</h3>
-        <div className={classnames(styles.blockRow, styles.correlationMatrix)}><CorrelationMatrix
-          data={project.correlationMatrixData}
-          header={project.correlationMatrixHeader} /></div>
-      </div>} */}
+
+          {/*{this.isShow('variableImpact') && <div className={classnames(styles.block, styles.VariableImpact)}>*/}
+          {/*  {this.checkBox('variableImpact')}*/}
+          {/*  <h3 className={styles.blockTitle}>{EN.VariableImpact}</h3>*/}
+          {/*  <div className={styles.blockRow}><VariableImpact model={project.selectModel} /></div>*/}
+          {/*</div>}*/}
 
 
+          {/*{this.isShow('score') && <div className={classnames(styles.block, styles.score)}>*/}
+          {/*  {this.checkBox('score')}*/}
+          {/*  <h3 className={styles.blockTitle}>{EN.Charts} {project.problemType === 'Classification' && <small onClick={this.reset}> reset</small>}</h3>*/}
+          {/*  <div className={styles.blockRow}><Score models={[project.selectModel]} project={project} /></div>*/}
+          {/*</div>}*/}
 
-        {/*<div className={styles.modelResult}>*/}
-        {/*  {this.isShow('modelResult') && <h1 className={styles.title}>{EN.ModelResult}</h1>}*/}
-        {/*  {this.checkBox('modelResult')}*/}
-        {/*  {this.isShow('modelName') && <div className={classnames(styles.block, styles.marginZero)}>*/}
-        {/*    {this.checkBox('modelName')}*/}
-        {/*    <h3 className={styles.blockTitle}>{EN.ModelName}: <span className={styles.modelName}>{project.selectModel.name}</span></h3>*/}
-        {/*    /!* {project.problemType === 'Classification' && <div className={styles.blockRow}><ClassificationPerformance project={project} /></div>} *!/*/}
-        {/*    /!* {project.problemType === 'Regression' && <div className={classnames(styles.blockRow, styles.performance)}><RegressionPerformance project={project} /></div>} *!/*/}
-        {/*  </div>}*/}
-        {/*  {this.isShow('metrics') && <div className={classnames(styles.block, styles.VariableImpact)}>*/}
-        {/*    {this.checkBox('metrics')}*/}
-        {/*    <h3 className={styles.blockTitle}>{EN.Metrics}</h3>*/}
-        {/*    {project.problemType === 'Regression' && <div className={styles.metrics}>*/}
-        {/*      <div className={classnames(styles.metricsRow, styles.metricsHeader)}>*/}
-        {/*        <span className={styles.metricsCell}></span>*/}
-        {/*        <span className={styles.metricsCell}>R²</span>*/}
-        {/*        <span className={styles.metricsCell}>Adjusted R²</span>*/}
-        {/*        <span className={styles.metricsCell}>MSE</span>*/}
-        {/*        <span className={styles.metricsCell}>RMSE</span>*/}
-        {/*        <span className={styles.metricsCell}>NRMSE</span>*/}
-        {/*        <span className={styles.metricsCell}>MAE</span>*/}
-        {/*      </div>*/}
-        {/*      <div className={styles.metricsRow}>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Validation}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.r2}>{formatNumber(vs.r2)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.adjustR2}>{formatNumber(vs.adjustR2)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.mse}>{formatNumber(vs.mse)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.rmse}>{formatNumber(vs.rmse)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.nrmse}>{formatNumber(vs.nrmse)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.mae}>{formatNumber(vs.mae)}</span>*/}
-        {/*      </div>*/}
-        {/*      <div className={styles.metricsRow}>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Holdout}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.r2}>{formatNumber(hs.r2)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.adjustR2}>{formatNumber(hs.adjustR2)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.mse}>{formatNumber(hs.mse)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.rmse}>{formatNumber(hs.rmse)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.nrmse}>{formatNumber(hs.nrmse)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.mae}>{formatNumber(hs.mae)}</span>*/}
-        {/*      </div>*/}
-        {/*    </div>}*/}
-        {/*    {project.problemType === 'Classification' && <div className={styles.metrics}>*/}
-        {/*      <div className={classnames(styles.metricsRow, styles.metricsHeader)}>*/}
-        {/*        <span className={styles.metricsCell}></span>*/}
-        {/*        <span className={styles.metricsCell}>AUC</span>*/}
-        {/*        <span className={styles.metricsCell}>Cutoff</span>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Accuracy}</span>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Precision}</span>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Recall}</span>*/}
-        {/*        <span className={styles.metricsCell}>F1 Score</span>*/}
-        {/*        <span className={styles.metricsCell}>KS</span>*/}
-        {/*        <span className={styles.metricsCell}>{EN.LogLoss}</span>*/}
-        {/*      </div>*/}
-        {/*      <div className={styles.metricsRow}>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Validation}</span>*/}
-        {/*        <span className={styles.metricsCell} title={vs.auc}>{formatNumber(vs.auc)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roc.Threshold[fitIndex]}>{formatNumber(roc.Threshold[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={model.accValidation}>{formatNumber(model.accValidation)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roc.Precision[fitIndex]}>{formatNumber(roc.Precision[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roc.Recall[fitIndex]}>{formatNumber(roc.Recall[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roc.F1[fitIndex]}>{formatNumber(roc.F1[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roc.KS[fitIndex]}>{formatNumber(roc.KS[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roc.LOGLOSS[fitIndex]}>{formatNumber(roc.LOGLOSS[fitIndex])}</span>*/}
-        {/*      </div>*/}
-        {/*      <div className={styles.metricsRow}>*/}
-        {/*        <span className={styles.metricsCell}>{EN.Holdout}</span>*/}
-        {/*        <span className={styles.metricsCell} title={hs.auc}>{formatNumber(hs.auc)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roch.Threshold[fitIndex]}>{formatNumber(roch.Threshold[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={model.accHoldout}>{formatNumber(model.accHoldout)}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roch.Precision[fitIndex]}>{formatNumber(roch.Precision[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roch.Recall[fitIndex]}>{formatNumber(roch.Recall[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roch.F1[fitIndex]}>{formatNumber(roch.F1[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roch.KS[fitIndex]}>{formatNumber(roch.KS[fitIndex])}</span>*/}
-        {/*        <span className={styles.metricsCell} title={roch.LOGLOSS[fitIndex]}>{formatNumber(roch.LOGLOSS[fitIndex])}</span>*/}
-        {/*      </div>*/}
-        {/*      <div className={styles.titles}>*/}
-        {/*        <div className={styles.metricsTitle}>{EN.ConfusionMatrix}</div>*/}
-        {/*        <div className={styles.metricsTitle}>{EN.CostBased}</div>*/}
-        {/*      </div>*/}
-        {/*      <PredictTable model={model} yes={yes} no={no} project={project} />*/}
-        {/*    </div>}*/}
-        {/*  </div>}*/}
-        {/*  {this.isShow('variableImpact') && <div className={classnames(styles.block, styles.VariableImpact)}>*/}
-        {/*    {this.checkBox('variableImpact')}*/}
-        {/*    <h3 className={styles.blockTitle}>{EN.VariableImpact}</h3>*/}
-        {/*    /!*<div className={styles.blockRow}><VariableImpact model={project.selectModel} /></div>*!/*/}
-        {/*  </div>}*/}
-        {/*  {this.isShow('score') && <div className={classnames(styles.block, styles.score)}>*/}
-        {/*    {this.checkBox('score')}*/}
-        {/*    <h3 className={styles.blockTitle}>{EN.Charts} {project.problemType === 'Classification' && <small onClick={this.reset}> reset</small>}</h3>*/}
-        {/*    <div className={styles.blockRow}><Score models={[project.selectModel]} project={project} /></div>*/}
-        {/*  </div>}*/}
-        {/*  {this.isShow('processFlow') && <div className={classnames(styles.block, styles.processFlow)}>*/}
-        {/*    {this.checkBox('processFlow')}*/}
-        {/*    <h3 className={styles.blockTitle}>{EN.ModelProcessFlow}</h3>*/}
-        {/*    <div className={styles.blockRow}><ModelProcessFlow model={project.selectModel} /></div>*/}
-        {/*  </div>}*/}
-        {/*</div>*/}
+          {/*{this.isShow('processFlow') && <div className={classnames(styles.block, styles.processFlow)}>*/}
+          {/*  {this.checkBox('processFlow')}*/}
+          {/*  <h3 className={styles.blockTitle}>{EN.ModelProcessFlow}</h3>*/}
+          {/*  <div className={styles.blockRow}><ModelProcessFlow model={project.selectModel} /></div>*/}
+          {/*</div>}*/}
+        </div>
       </div>
-
     )
   }
 }
