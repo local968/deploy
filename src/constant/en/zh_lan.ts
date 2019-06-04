8c1015d918e14c6fed1de05e594462114a2c0dc0
zl <zl940722@gmail.com>
2019-06-04 14:00:48 +0800

    update

--- src/constant/en/zh_lan.ts
+++ src/constant/en/zh_lan.ts
@@ -967,7 +967,7 @@ const ZH_LAN: any = {
   clusters: '聚类类别',
   ImportantVariables: '重要变量',
   Cluster: '聚类类别',
-  ClusteringHint: '对样本进行分类，把*相似样本*归为同一类。（例如：客户分群与画像）',
+  ClusteringHint: '对样本进行分类，把相似样本归为同一类。（例如：客户分群与画像）',
   OutlierDetectionHint: '识别与其他样本差异过大的低频、可疑样本。（例如：信用卡欺诈行为侦测）',
   ExplainationHint: '展示对每一个聚类类别重要的变量。',
   Doyouwanttotreatnull: '是否把缺失值设置为有效类别？',
