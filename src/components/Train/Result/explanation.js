09cfe8940b89f0ca1fae045328460e29e6c228d1
congcong <congcong.dai@r2.ai>
2019-07-02 11:19:12 +0800

    fix Explanation

--- src/components/Train/Result/explanation.js
+++ src/components/Train/Result/explanation.js
@@ -2,6 +2,7 @@ import classes from "./styles.module.css";
 import { Table } from "components/Common";
 import React from 'react'
 import EN from '../../../constant/en';
+// import { formatNumber } from 'util'
 const Explanation = (props) => {
   const { model, mapHeader } = props;
   const { labelWithImportance } = model
@@ -10,9 +11,10 @@ const Explanation = (props) => {
     const indexes = [{ content: <span title={EN.ImportantVariables}>{EN.ImportantVariables}</span>, cn: classes.explanationCell }]
     const values = [{ content: <span title={EN.ImportantValues}>{EN.ImportantValues}</span>, cn: classes.explanationCell }]
     Object.entries(labelWithImportance).map(([k, v]) => {
+      const curValues = v.values.map(v => isNaN(+v) ? v : Math.round(parseFloat(v.toString()) * 1000) / 1000)
       fields.push({ content: <span title={k}>{k}</span>, cn: classes.explanationCell })
       indexes.push({ content: <span title={v.indexes.map(_i => mapHeader[_i]).join(',')}>{v.indexes.map(_i => mapHeader[_i]).join(',')}</span>, cn: classes.explanationCell })
-      values.push({ content: <span title={v.values.join(',')}>{v.values.join(',')}</span>, cn: classes.explanationCell })
+      values.push({ content: <span title={curValues.join(',')}>{curValues.join(',')}</span>, cn: classes.explanationCell })
     })
     return [fields, indexes, values]
   })
