db550ad602018887d573870a04248bdfbce2211b
zl <lin.zhou@r2.ai>
2019-07-02 17:38:30 +0800

    fix bug

--- src/components/Report/Summary/index.js
+++ src/components/Report/Summary/index.js
@@ -7,6 +7,7 @@ import EN from '../../../constant/en';
 import {
   PIE
 } from "../../Charts"
+import classnames from "classnames";
 @observer
 class Summary extends Component {
   componentDidMount() {
@@ -20,106 +21,78 @@ class Summary extends Component {
 
   startTrain = () => {
     const { project } = this.props
-    project.updateProject(project.nextMainStep(3))
+    project.updateProject({ ...project.nextMainStep(3), runWith: project.totalLines < 10000 ? 'cross' : 'holdout' })
   }
 
   renderD3 = () => {
     d3.select(`.${styles.summaryChart} svg`).remove();
 
-    const outerRadius = 100;           // 外半径
+    const outerRadius = 60;           // 外半径
     const innerRadius = 0;             // 内半径
     //弧生成器
     const arc = d3.arc()
-      .innerRadius(innerRadius)
-      .outerRadius(outerRadius)
-    const { totalRawLines, totalLines, totalFixedLines } = this.props.project
-    const deleteRows = totalRawLines - totalLines
-    const fixedRows = totalFixedLines
-    const cleanRows = totalLines - totalFixedLines
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius)
+    const { totalRawLines, deletedCount, totalFixedLines } = this.props.project
+    const deleteRows = deletedCount
+    const fixedRows = totalFixedLines - deletedCount
+    const cleanRows = totalRawLines - totalFixedLines
     const data = [fixedRows, deleteRows, cleanRows]
     const color = ['#9cebff', '#c4cbd7', '#00c855'];
     const dataset = d3.pie()(data);
 
     const svg = d3.select(`.${styles.summaryChart}`)
-      .append("svg")
-      .attr("width", 200)
-      .attr("height", 200)
+        .append("svg")
+        .attr("width", 120)
+        .attr("height", 120)
 
     svg.selectAll(`g`)
-      .data(dataset)
-      .enter()
-      .append("g")
-      .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")
-      .append("path")
-      .attr("fill", (d, i) => color[i])
-      .attr("d", (d) => {
-        return arc(d);   //调用弧生成器，得到路径值
-      });
+        .data(dataset)
+        .enter()
+        .append("g")
+        .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")")
+        .append("path")
+        .attr("fill", (d, i) => color[i])
+        .attr("d", (d) => {
+          return arc(d);   //调用弧生成器，得到路径值
+        });
   }
 
   render() {
-    const { project } = this.props;
-    const { target, colType, sortHeader, dataHeader, totalRawLines, nullLineCounts, mismatchLineCounts, outlierLineCounts, totalFixedLines, problemType, mismatchFillMethod, nullFillMethod, outlierFillMethod,
-      deletedCount,
-      variableIssues: { nullRow, mismatchRow, outlierRow },
-      variableIssueCount: { nullCount, mismatchCount, outlierCount },
-      totalLines,
-      issues,
-    } = project;
-    const deletePercent = formatNumber(deletedCount / totalRawLines * 100, 2);
-    const fixedPercent = formatNumber((totalFixedLines - deletedCount) / totalRawLines * 100, 2);
-    const cleanPercent = formatNumber(100 - deletePercent - fixedPercent, 2);
-    
-    const currentHeader = sortHeader.filter(h => dataHeader.includes(h));
-    // const variableList = currentHeader.slice(1);
-    const variableList = dataHeader;
-    // const percentList = currentHeader.map(v => {
-    //   const percent = {
-    //     missing: (nullLineCounts[v] || 0) / (totalRawLines || 1) * 100,
-    //     mismatch: (mismatchLineCounts[v] || 0) / (totalRawLines || 1) * 100,
-    //     outlier: (problemType !== 'Classification' ? (outlierLineCounts[v] || 0) : 0) / (totalRawLines || 1) * 100
-    //   };
-    //   percent.clean = 100 - percent.missing - percent.mismatch - percent.outlier;
-    //   return percent
-    // });
-    const percentList = dataHeader.map(v => {
+    const { project, editFixes } = this.props;
+    const { mapHeader, target, sortHeader, colType, dataHeader, totalRawLines, deletedCount, totalLines, targetIssuesCountsOrigin, variableIssueCount: { nullCount, mismatchCount, outlierCount }, variableIssues: { nullRow, mismatchRow, outlierRow }, totalFixedLines, problemType, issues } = project
+    const deletePercent = formatNumber(deletedCount / totalRawLines * 100, 2)
+    const fixedPercent = formatNumber((totalFixedLines - deletedCount) / totalRawLines * 100, 2)
+    const cleanPercent = formatNumber(100 - deletePercent - fixedPercent, 2)
+    const currentHeader = sortHeader.filter(h => dataHeader.includes(h))
+    const variableList = currentHeader.slice(1)
+    const percentList = currentHeader.map(v => {
+      const isNum = colType[v] === 'Numerical'
       const percent = {
         missing: nullRow[v] || 0,
         mismatch: mismatchRow[v] || 0,
         outlier: outlierRow[v] || 0
-      };
-      percent.clean = 100 - percent.missing - percent.mismatch - percent.outlier;
+      }
+      percent.clean = 100 - percent.missing - percent.mismatch - percent.outlier
       return percent
-    });
-    console.log('percentList',percentList,project)
-    const fillMethod = method => method === 'ignore' || method === 'drop' ? method : `replace with ${method}`;
-    const getFixMethod = key => {
-      let result = [];
-      const mismatchDefault = colType[key] === 'Numerical' ? 'mean' : 'mode';
-      const nullDefault = colType[key] === 'Numerical' ? 'mean' : 'mode';
-      const outlierDefault = colType[key] === 'drop'
-      if (mismatchLineCounts[key]) result.push(`mismatch: ${fillMethod(mismatchFillMethod[key] || mismatchDefault)}`)
-      if (nullLineCounts[key]) result.push(`missing value: ${fillMethod(nullFillMethod[key] || nullDefault)}`)
-      if (outlierLineCounts[key]) result.push(`outlier: ${fillMethod(outlierFillMethod[key] || outlierDefault)}`)
-      return result.join('\n')
-    }
+    })
     return <div className={styles.summary}>
       <div className={styles.summaryLeft}>
         <div className={styles.summaryTitle}><span>{EN.Summaryofyourdata}</span></div>
         <div className={styles.summaryTypeBox}>
-          {<div className={styles.summaryType}>
+          <div className={styles.summaryType}>
             <div className={styles.summaryCube} style={{ backgroundColor: '#00c855' }} />
             <span>{EN.CleanData}</span>
-          </div>}
-          {!!mismatchCount && <div className={styles.summaryType}>
+          </div>
+          {(!!targetIssuesCountsOrigin.mismatchRow || !!mismatchCount) && <div className={styles.summaryType}>
             <div className={styles.summaryCube} style={{ backgroundColor: '#819ffc' }} />
             <span>{EN.DataTypeMismatch}</span>
           </div>}
-          {!!nullCount && <div className={styles.summaryType}>
+          {(!!targetIssuesCountsOrigin.nullRow || !!nullCount) && <div className={styles.summaryType}>
             <div className={styles.summaryCube} style={{ backgroundColor: '#ff97a7' }} />
             <span>{EN.MissingValue}</span>
           </div>}
-          {(problemType !== 'Outlier' && !!outlierCount) && <div className={styles.summaryType}>
+          {(problemType !== 'Classification' && (!!targetIssuesCountsOrigin.outlierRow || !!outlierCount)) && <div className={styles.summaryType}>
             <div className={styles.summaryCube} style={{ backgroundColor: '#f9cf37' }} />
             <span>{EN.Outlier}</span>
           </div>}
@@ -131,7 +104,7 @@ class Summary extends Component {
               <div className={styles.summaryCell}><span style={{ fontWeight: 'bold' }}>{EN.CleanData}</span></div>
             </div>
             <div className={styles.summaryTableRow}>
-              <div className={styles.summaryCell}><span>{target}</span></div>
+              <div className={styles.summaryCell}><span>{mapHeader[target]}</span></div>
               <div className={styles.summaryCell}><span>{formatNumber(percentList[0].clean, 2)}%</span></div>
             </div>
           </div>
@@ -149,7 +122,7 @@ class Summary extends Component {
             </div>
           </div>
         </div>
-        <div className={styles.summaryTable}>
+        <div className={styles.summaryTable} style={{ paddingRight: '.2em' }}>
           <div className={styles.summaryTableLeft}>
             <div className={styles.summaryTableRow}>
               <div className={styles.summaryCell}><span style={{ fontWeight: 'bold' }}>{EN.PredictorVariables}</span></div>
@@ -162,25 +135,25 @@ class Summary extends Component {
             </div>
           </div>
         </div>
-        <div className={styles.summaryTable} style={{ maxHeight: '4em' }}>
+        <div className={styles.summaryTable} style={{ overflow: 'scroll' }}>
           <div className={styles.summaryTableLeft}>
             {variableList.map((v, k) => {
-              const percent = percentList[k]
+              const percent = percentList[k + 1]
               return <div className={styles.summaryTableRow} key={k}>
-                <div className={styles.summaryCell}><span>{v}</span></div>
+                <div className={styles.summaryCell}><span>{mapHeader[v]}</span></div>
                 <div className={styles.summaryCell}><span>{formatNumber(percent.clean, 2)}%</span></div>
               </div>
             })}
           </div>
           <div className={styles.summaryTableRight}>
             {variableList.map((v, k) => {
-              const percent = percentList[k]
+              const percent = percentList[k + 1]
               return <div className={styles.summaryTableRow} key={k}>
                 <div className={styles.summaryProgressBlock}>
-                  <div className={styles.summaryProgress} style={{width: percent.clean + '%', backgroundColor: '#00c855'}}/>
-                  <div className={styles.summaryProgress} style={{width: percent.mismatch + '%', backgroundColor: '#819ffc'}}/>
-                  <div className={styles.summaryProgress} style={{width: percent.missing + '%', backgroundColor: '#ff97a7'}}/>
-                  <div className={styles.summaryProgress} style={{width: percent.outlier + '%', backgroundColor: '#f9cf37'}}/>
+                  <div className={styles.summaryProgress} style={{ width: percent.clean + '%', backgroundColor: '#00c855' }} />
+                  <div className={styles.summaryProgress} style={{ width: percent.mismatch + '%', backgroundColor: '#819ffc' }} />
+                  <div className={styles.summaryProgress} style={{ width: percent.missing + '%', backgroundColor: '#ff97a7' }} />
+                  {problemType !== 'Classification' && <div className={styles.summaryProgress} style={{ width: percent.outlier + '%', backgroundColor: '#f9cf37' }} />}
                 </div>
               </div>
             })}
@@ -200,36 +173,45 @@ class Summary extends Component {
           <div className={styles.summaryParts}>
             <div className={styles.summaryPart}>
               <div className={styles.summaryPartText}>
-                <div className={styles.summaryCube} style={{backgroundColor: '#9cebff'}}/>
+                <div className={styles.summaryCube} style={{ backgroundColor: '#9cebff' }} />
                 <span style={{ fontWeight: 'bold' }}>{EN.RowsWillBeFixed}</span>
               </div>
               <div className={styles.summaryPartText}>
-                <div className={styles.summaryCube}/>
+                <div className={styles.summaryCube} />
                 <span>{fixedPercent}%</span>
               </div>
             </div>
             <div className={styles.summaryPart}>
               <div className={styles.summaryPartText}>
-                <div className={styles.summaryCube} style={{backgroundColor: '#c4cbd7'}}/>
+                <div className={styles.summaryCube} style={{ backgroundColor: '#c4cbd7' }} />
                 <span style={{ fontWeight: 'bold' }}>{EN.RowsWillBeDeleted}</span>
               </div>
               <div className={styles.summaryPartText}>
-                <div className={styles.summaryCube}/>
+                <div className={styles.summaryCube} />
                 <span>{deletePercent}%</span>
               </div>
             </div>
             <div className={styles.summaryPart}>
               <div className={styles.summaryPartText}>
-                <div className={styles.summaryCube} style={{backgroundColor: '#00c855'}}/>
+                <div className={styles.summaryCube} style={{ backgroundColor: '#00c855' }} />
                 <span style={{ fontWeight: 'bold' }}>{EN.CleanData}</span>
               </div>
               <div className={styles.summaryPartText}>
-                <div className={styles.summaryCube}/>
+                <div className={styles.summaryCube} />
                 <span>{cleanPercent}%</span>
               </div>
             </div>
           </div>
         </div>
+        {/*<div className={styles.summaryBottom}>*/}
+        {/*  <div className={classnames(styles.summaryButton, styles.summaryConfirm, {*/}
+        {/*    [styles.disabled]: totalLines === 0*/}
+        {/*  })} onClick={totalLines === 0 ? null : this.startTrain}><span>{EN.Continue}</span></div>*/}
+        {/*  <div className={classnames(styles.summaryButton, {*/}
+        {/*    [styles.disabled]: !issues.dataIssue*/}
+        {/*  })} onClick={issues.dataIssue ? editFixes : null}><span>{EN.EditTheFixes}</span></div>*/}
+        {/*  <div className={styles.summaryButton} onClick={this.backToConnect}><span>{EN.LoadaBetterDataset}</span></div>*/}
+        {/*</div>*/}
       </div>
     </div>
   }
