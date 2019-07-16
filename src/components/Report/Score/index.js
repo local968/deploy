1979ebeb6e591036c9747f90e42f55619b9f862d
zl <lin.zhou@r2.ai>
2019-07-16 16:26:26 +0800

    update report

--- src/components/Report/Score/index.js
+++ src/components/Report/Score/index.js
@@ -864,7 +864,8 @@ class PredictTable extends Component {
                 <dt>
                   <div className={styles.eventInput}>
                     <span style={{ marginRight: '0.5em' }}>{EN.EventDistribution}</span>
-                    <NumberInput value={typeof this.distribution === 'number' ? this.distribution : (event / 100)} onBlur={this.handleChangeEvent} min={0.00} max={100.00} isInt={false} digits={2} cut={true} />
+                    {/*<NumberInput value={typeof this.distribution === 'number' ? this.distribution : (event / 100)} onBlur={this.handleChangeEvent} min={0.00} max={100.00} isInt={false} digits={2} cut={true} />*/}
+                    <NumberInput value={typeof this.distribution === 'number' ? this.distribution : (event / 100)}  min={0.00} max={100.00} isInt={false} digits={2} cut={true} />
                     <span style={{ marginLeft: '0.5em' }}>%</span>
                     {/*<span style={{ marginLeft: '10px' }}><a className={styles.reset} onClick={this.reset}>{EN.Reset}</a></span>*/}
                   </div>
